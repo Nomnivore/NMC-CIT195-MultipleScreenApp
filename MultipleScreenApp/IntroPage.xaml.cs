c61031180d714ae98b2c3eb850f613f9168b37c9
Nomnivore <6979410+Nomnivore@users.noreply.github.com>
2022-03-03 15:32:40 -0500

    Add project files.

---
+++ MultipleScreenApp/IntroPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MultipleScreenApp
+{
+    /// <summary>
+    /// Interaction logic for IntroPage.xaml
+    /// </summary>
+    public partial class IntroPage : Page
+    {
+        public IntroPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new SoloPage());
+        }
+    }
+}
---
+++ MultipleScreenApp/SoloPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MultipleScreenApp
+{
+    /// <summary>
+    /// Interaction logic for SoloPage.xaml
+    /// </summary>
+    public partial class SoloPage : Page
+    {
+        public SoloPage()
+        {
+            InitializeComponent();
+        }
+
+        private void NextBtn_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new TeamPage());
+        }
+
+        private void PrevBtn_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new IntroPage());
+        }
+    }
+}
